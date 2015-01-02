c87c835b34c69c5d712203ece2229b3971fb2ba4
itay88 <mogoby123@Gmail.com>
2015-01-02 21:11:39 +0200

    Update itay.py

--- itay.py
+++ itay.py
@@ -1,9 +1,20 @@
 Class itay():
-  def itay(x):
+  def Itay(x):
     if x == 5341:
       print "v"
     else:
       if x > 5340:
         print "המספר גדול יותר"
       else:
-        print "המספר קטן יותר"
+        if x < 5342:
+         print "המספר קטן יותר"
+Class itay():
+  def Itay(x):
+    if x == 5331:
+      print "v"
+    else:
+      if x > 5330:
+        print "המספר גדול יותר"
+      else:
+        if x < 5332:
+          print "המספר קטן יותר"
